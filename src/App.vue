c5b15cf3eb676e017cd310d893cbacdb9e9ae36e
DESKTOP-RPU80GU\gn01792218 <gn01792218@hotmail.com>
2022-06-28 10:16:33 +0800

    上傳專案

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app">
+    <Home/>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Home from '@/views/Home.vue'
+export default defineComponent({
+  components: {
+    Home,
+  },
+});
+</script>
+
+<style lang="scss">
+  .app{
+    width:1280px;
+    max-height: 100vh;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+</style>
---
+++ src/store/index.ts
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex'
+import lobby from './lobby'
+import auth from './auth'
+import table from './table'
+
+export default createStore({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    lobby,auth,table,
+  }
+})
